4ea68c12f05c34840b11444a0c61ea8f6cbcd686
Erick Novaes56 <86386003+ericknovaes56@users.noreply.github.com>
2022-11-02 14:58:48 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const startBtn = document.querySelector('#start');
+const output = document.querySelector('#output');
+const stopvoice = document.getElementById("stop")
+const salvar = document.getElementById("salvar")
+const alink = document.getElementById("alink")
+var texts = ''
+startBtn.addEventListener("click", ()=>{
+    startBtn.style.display='none'
+    stopvoice.style.display='flex'
+    const recognition = new webkitSpeechRecognition();
+    recognition.interimResults = true;
+    recognition.lang = "pt-br";
+    recognition.continuous = true;
+    recognition.start();
+    recognition.onresult = function(event) {
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        if (event.results[i].isFinal) {
+            const content = event.results[i][0].transcript.trim();
+            if (content == 'YouTube'){
+                window.location.href='https://www.youtube.com/'
+                output.classList.add("ok")
+            }
+            output.textContent = content;
+            salvar.style.display='flex'
+            recognition.stop();
+            startBtn.style.display='flex'
+            stopvoice.style.display='none'
+
+            texts = content
+        }
+      }
+    };
+    stopvoice.addEventListener("click", ()=>{
+        recognition.stop();
+        startBtn.style.display='flex'
+        stopvoice.style.display='none'
+    })
+    salvar.addEventListener("click", ()=>{
+        let blob = new Blob([texts],{
+            type: "text/plain;charset=utf-8"
+        })
+        var link = window.URL.createObjectURL(blob);
+        alink.href = link
+        alink.download = 'Salvo'
+    })
+})
